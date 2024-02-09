aba0b5cfbfc0712a5812404e9f9563b8d06ec5dd
0xNonCents <0xNonCents@gmail.com>
2024-02-09 15:11:22 -0500

    Modify calculateGameProgress and calculateTimeLeftInStage

--- react-app/src/hooks/useGameState.tsx
+++ react-app/src/hooks/useGameState.tsx
@@ -8,7 +8,6 @@ import { useQueryParams } from "./useQueryParams";
 const GAME_ID_CONFIG = 2096807177358152958712390915352935n;
 const STAGES = ["Commit", "Reveal", "Resolve"];
 const STAGE_COLOURS = ["bg-blue-500", "bg-green-500", "bg-red-200"];
-const CYCLE_UNITS = 8;
 const SECONDS_IN_DAY = 86400;
 const SECONDS_IN_HOUR = 3600;
 const SECONDS_IN_MINUTE = 60;
@@ -33,7 +32,8 @@ export const useGameState = () => {
 
   const { hoursLeft, minutesLeft, secondsLeft } = calculateTimeLeftInStage(
     game,
-    elapsedTime
+    elapsedTime,
+    currentStageIndex
   );
 
   const isSpawnCycle = totalCycles == 0 ? false : totalCycles % 2 === 0;
@@ -79,8 +79,18 @@ const calculateGameProgress = (game: any, elapsedTime: number) => {
   const totalDays = Math.floor(elapsedTime / SECONDS_IN_DAY);
   const totalHours = Math.floor(elapsedTime / SECONDS_IN_HOUR);
   const totalCycleUnits = Math.floor(elapsedTime / game?.cycle_unit);
-  const currentStageIndex =
-    Math.floor(totalCycleUnits / CYCLE_UNITS) % STAGES.length;
+
+  // A day measured in cycles is determined by the length of each stage
+  const cycleUnitsOfCurrentYear = totalCycleUnits % (game.commit_length + game.reveal_length + game.resolve_length);
+  
+  //Default is Commit stage (STAGES[0])
+  let currentStageIndex = 0;
+  if (cycleUnitsOfCurrentYear > game.commit_length) {
+    currentStageIndex = 1;
+  } 
+  if (cycleUnitsOfCurrentYear > (game.reveal_length + game.commit_length)) {
+    currentStageIndex = 2;
+  };
   const totalCycles = Math.floor(
     totalCycleUnits /
       (game?.commit_length + game?.reveal_length + game?.resolve_length)
@@ -89,10 +99,20 @@ const calculateGameProgress = (game: any, elapsedTime: number) => {
   return { totalDays, totalHours, currentStageIndex, totalCycles };
 };
 
-const calculateTimeLeftInStage = (game: any, elapsedTime: number) => {
-  const secondsLeftInStage =
-    CYCLE_UNITS * game?.cycle_unit -
-    (elapsedTime % (CYCLE_UNITS * game?.cycle_unit));
+const calculateTimeLeftInStage = (game: any, elapsedTime: number, stageIndex : number) => {
+  //Gets the total seconds in the current year(in-game year which is playing through each stage)
+  const commitLengthSeconds = game.commit_length * game.cycle_unit;
+  const revealLengthSeconds = game.reveal_length * game.cycle_unit;
+  const resolveLengthSeconds = game.resolve_length * game.cycle_unit;
+
+  const secondsInCurrentYear =  elapsedTime % (commitLengthSeconds + revealLengthSeconds + resolveLengthSeconds);
+  
+  let secondsLeftInStage = commitLengthSeconds - secondsInCurrentYear
+  if(stageIndex === 1) {
+    secondsLeftInStage = commitLengthSeconds + revealLengthSeconds - secondsInCurrentYear
+  } else if(stageIndex === 2) {
+    secondsLeftInStage = commitLengthSeconds + revealLengthSeconds + resolveLengthSeconds - secondsInCurrentYear
+  }
   const hoursLeft = Math.floor(secondsLeftInStage / SECONDS_IN_HOUR);
   const minutesLeft = Math.floor(
     (secondsLeftInStage % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
